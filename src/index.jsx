3cde7f49d0d4a9cd26b2c2992accca9edb41b725
itanvirdev <thisistanvir.dev@gmail.com>
2023-08-28 06:33:53 +0600

    Class Component

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from 'react-dom/client';
 
-function Clock({locale}){
-  return(
-    <h1 className="heading">
-      <span className="text">Hello {new Date().toLocaleDateString(locale)}</span>
-    </h1>
-  )
+class Clock extends React.Component{
+  render(){
+    return(
+      <h1 className="heading">
+        <span className="text">Hello - {this.props.children} {new Date().toLocaleDateString(this.props.locale)}</span>
+      </h1>
+    );
+  }
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Clock locale="bn-BD"/>);
+ReactDOM.createRoot(document.getElementById('root')).render(<Clock locale="bn-BD">test</Clock>);
 
