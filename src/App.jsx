258a3633d11a6076ad1e3a09985a813c351ee831
itanvirdev <thisistanvir.dev@gmail.com>
2023-10-12 06:22:42 +0600

    Done lesson 25 Stylesheets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import LayoutComponentOne from "./components/LayoutComponentOne"
+import Logo from "./components/Logo"
+
+import "./assets/css/global.css"
 
 function App() {
 
   return (
     <div className="app">
-      <LayoutComponentOne/>
+      <h1>Learn With Sumit</h1>
+      <Logo/>
     </div>
   )
 
